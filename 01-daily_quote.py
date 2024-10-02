82c1d054b9862d8c8f5b9548f6eb29a68849b0d7
SHWETHA KANDHALU <shwetha.kandhalubhaskar@ucsf.edu>
2024-10-01 23:15:37 -0700

    daily quote done

--- 01-daily_quote.py
+++ 01-daily_quote.py
@@ -14,16 +14,20 @@ Hint: Look up `random.choice()` to select a random item from a list. You can use
 import random
 from datetime import date
 
-
 quotes = [
     # Create a list of quotes here
+    "The man who says his wife can’t take a joke, forgets that she took him. — Oscar Wilde",
+    "All you need is love. But a little chocolate now and then doesn’t hurt. ― Charles M. Schulz",
+    "Instant gratification takes too long. ― Carrie Fisher",
+    "Don’t be so humble — you are not that great. ― Golda Meir",
+    "Why do they call it rush hour when nothing moves? — Robin Williams"
 ]
 
 def get_quote_of_the_day(quotes):
     todays_quote = None
 
     # Your code here
-    
+    todays_quote = random.choice(quotes)
     return todays_quote
 
 if __name__ == "__main__":
